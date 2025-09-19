57c004964113c833d16b13916009216b3cfa8494
Abhishek Gupta <abhishek31052003@gmail.com>
2025-09-19 10:54:39 +0530

    food Partner authentication

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const foodPartnerModel = require("../models/food-partner.model");
 
 // Register user
 const registerUser = async (req, res) => {
@@ -106,4 +107,110 @@ const logoutUser = (req, res) => {
   });
 };
 
-module.exports = { registerUser, loginUser, logoutUser };
+// register food-partner
+const registerFoodPartner = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+    const isPartnerAlreadyExists = await foodPartnerModel.findOne({ email });
+    if (isPartnerAlreadyExists) {
+      return res.status(400).json({
+        success: false,
+        message: "Food partner already registered",
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const partner = await foodPartnerModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = jwt.sign(
+      {
+        partnerId: partner._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+    res.cookie("token", token);
+    res.status(201).json({
+      success: true,
+      message: "Food partner registered successfully",
+      partner: {
+        partnerId: partner._id,
+        name: partner.name,
+        email: partner.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// login food-partner
+const loginFoodPartner = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+    const partner = await foodPartnerModel.findOne({
+      email,
+    });
+    if (!partner) {
+      return res.status(400).json({
+        success: false,
+        message: "Food partner not registered",
+      });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, partner.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+    const token = jwt.sign({ partnerId: partner._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.cookie("token", token);
+    res.status(200).json({
+      success: true,
+      message: "Food partner logged in successfully",
+      partner: {
+        partnerId: partner._id,
+        name: partner.name,
+        email: partner.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// logout food-partner
+const logoutFoodPartner = (req, res) => {
+  res.clearCookie("token");
+  res.status(200).json({
+    success: true,
+    message: "Food partner logged out successfully",
+  });
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+  logoutUser,
+  registerFoodPartner,
+  loginFoodPartner,
+  logoutFoodPartner,
+};
